120723f2b503175c7d17b2d417caf798928444ce
tpy001 <3414395729@qq.com>
2024-05-20 01:11:15 +0000

    hrda head

--- rein/models/heads/__init__.py
+++ rein/models/heads/__init__.py
@@ -1,3 +1,5 @@
 from .rein_mask2former import ReinMask2FormerHead
+from .hrda import HRDAHead
 
-__all__ = ["ReinMask2FormerHead"]
+__all__ = ["ReinMask2FormerHead",
+           "HRDAHead"]
--- rein/utils/__init__.py
+++ rein/utils/__init__.py
@@ -1,1 +1,3 @@
 from .visualization import subplotimg
+from .misc import add_prefix
+from .wrappers import resize,crop
