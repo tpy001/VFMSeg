575644532f90a0535c20fb8de9f7ef0a367cd3fc
tpy001 <3414395729@qq.com>
2024-08-09 10:49:40 +0000

    rein+eva02. Model perform poorly without RCS, the result in mIoU is only 44.5%

---
+++ configs/dg/gta2citys/dg_rein_eva02_mask2former_512x512_bs1x4.py
@@ -0,0 +1,42 @@
+# dataset config
+_base_ = [
+    "../datasets/dg_gta2citys_512x512.py",
+    "../../_base_/default_runtime.py",
+    "../../_base_/models/rein_eva02_mask2former.py",
+]
+
+embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
+optim_wrapper = dict(
+    constructor="PEFTOptimWrapperConstructor",
+    optimizer=dict(
+        type="AdamW", lr=0.0001, weight_decay=0.05, eps=1e-8, betas=(0.9, 0.999)
+    ),
+    paramwise_cfg=dict(
+        custom_keys={
+            "norm": dict(decay_mult=0.0),
+            "query_embed": embed_multi,
+            "level_embed": embed_multi,
+            "learnable_tokens": embed_multi,
+            "reins.scale": embed_multi,
+        },
+        norm_decay_mult=0.0,
+    ),
+)
+param_scheduler = [
+    dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=40000, by_epoch=False)
+]
+
+
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=40000, val_interval=4000)
+val_cfg = dict(type="ValLoop")
+test_cfg = dict(type="TestLoop")
+default_hooks = dict(
+    timer=dict(type="IterTimerHook"),
+    logger=dict(type="LoggerHook", interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type="ParamSchedulerHook"),
+    checkpoint=dict(
+        type="CheckpointHook", by_epoch=False, interval=4000, max_keep_ckpts=3
+    ),
+    sampler_seed=dict(type="DistSamplerSeedHook"),
+    visualization=dict(type="SegVisualizationHook"),
+)
